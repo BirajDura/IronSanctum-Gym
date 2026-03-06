0d5494c6c0d0fcaa443af8bb42e71cd9310457be
Biraj Dura <durabiraj220@gmail.com>
2026-03-06 21:28:30 +0545

    New file added

---
+++ src/components/Service.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div>
+      <p>This is the service page with all the suppoet </p>
+    </div>
+  )
+}
+
+export default Service
